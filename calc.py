b468b6e9d186ff4971a96ef09309a540c5350c61
potamovaevgeniya <enia887@mail.ru>
2023-03-15 23:24:40 +0500

    1

---
+++ calc.py
@@ -0,0 +1,13 @@
+#Простые вычисления
+def sum (x,y):
+    return x+y
+def sub (x,y):
+    return x-y
+def multiply (x,y):
+    return x*y
+def divint (x,y):
+    return x/y
+print (sum(1,3))
+print (sub(5,3))
+print (multiply(2,2))
+print (divint(10,2))
