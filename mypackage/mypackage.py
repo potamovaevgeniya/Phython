1f3cf87925a0354ca022a59713a2eda53a01fbff
potamovaevgeniya <enia887@mail.ru>
2023-03-15 23:25:19 +0500

    1

--- mypackage/mypackage.py
+++ mypackage/mypackage.py
@@ -1,9 +1,9 @@
-#импорт модуля
+#импорт функций из модуля
 from calc import sum
 from calc import sub
 from calc import multiply
 from calc import divint
-print (sum (2,3))
+print (sum(2,3))
 print (sub(5,3))
 print (multiply(2,2))
 print (divint(10,2))
